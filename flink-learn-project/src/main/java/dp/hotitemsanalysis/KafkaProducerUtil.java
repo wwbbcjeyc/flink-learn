060ea8d616c038faeed4777fb7707994a6595731
wangwenbo <github.com>
2021-02-02 23:43:19 +0800

    hotsql

---
+++ flink-learn-project/src/main/java/dp/hotitemsanalysis/KafkaProducerUtil.java
@@ -0,0 +1,46 @@
+package dp.hotitemsanalysis;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @Author wangwenbo
+ * @Date 2021/2/2 11:18 下午
+ * @Version 1.0
+ */
+public class KafkaProducerUtil {
+    public static void main(String[] args) throws IOException {
+        writeToKafka("hotitems");
+    }
+
+    // 包装一个写入kafka的方法
+    private static void writeToKafka(String topic) throws IOException {
+        // kafka 配置
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        // 定义一个Kafka Producer
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        // 用缓冲方式读取文本
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("/Users/wangwenbo/Data/UserBehavior.csv")
+        );
+        String line;
+        while( (line = bufferedReader.readLine()) != null ){
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, line);
+            // 用producer发送数据
+
+            kafkaProducer.send(producerRecord);
+        }
+        kafkaProducer.close();
+
+    }
+}
